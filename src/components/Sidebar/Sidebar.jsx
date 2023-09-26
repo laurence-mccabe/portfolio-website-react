0bffba042a2b58186f02bf1db3e97d0e05d9c8fc
laurence0202 <laurence0202@users.noreply.github.com>
2023-09-26 17:05:44 +1000

    created initial files for project, sidebar, installed bootstrap

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,18 @@
+import './Sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <div className="container-fluid sidebar-section">
+
+      <div className='sidebar'>
+      </div>
+
+      <div className='container'>
+        <p>Home section</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
